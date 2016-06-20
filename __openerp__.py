ba0430d865afdb40b6f7d0f9dcd30f6494d2ab7b
Rob Burgertech <Burger@bogen.local>
2016-06-20 10:58:23 +0200

    Created template files and added basic information so that I can reuse the template and not have to create new files every time a create a new module

---
+++ __openerp__.py
@@ -0,0 +1,9 @@
+{
+	'name' : 'Module Template',
+	'description' : 'Use this template to create a barebones module that does not require manual setup every time you want to create a new custom module.',
+	'author' : 'Rob Burger',
+	'depends' : 'base',
+	'summary' : '',
+	'website' : 'www.github.com',
+	#'data' : []
+}
